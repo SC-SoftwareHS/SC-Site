96eb59ec4fa42ec971c7ac07c3ebb13342d9f151
Harrison Stoneham <harrison@sylvanitecapital.com>
2025-12-11 10:33:27 -0800

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,10 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Sylvanite Capital",
   description: "A private investment firm that makes long-term investments in small and lower middle-market businesses.",
+  icons: {
+    icon: "/favicon.png",
+    apple: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
